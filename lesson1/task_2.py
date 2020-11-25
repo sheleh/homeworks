d283b0ad4402487da9bfb751038c51126c880e69
Di3X <sheleh@Di3Xs-MacBook-Pro.local>
2020-11-25 20:23:56 +0200

    first

---
+++ lesson1/task_2.py
@@ -0,0 +1,7 @@
+#lesson_#1
+# using print()
+print('hello world!')
+print('hello world!'*5, sep = '_')
+print('hello world!','hello sun!','hello moon!' ,sep = '#')
+print('hello world!', 'hello sun!', 'hello moon', sep='\n++++++++++\n', 
+      end = '\n\t======THE END======')
