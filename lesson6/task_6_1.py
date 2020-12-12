5a742efbb15fd5c15eb9089ba3de907cae6617d5
sheleh <shelex87@gmail.com>
2020-12-12 09:52:43 +0200

    1 and 3 is reloaded

--- lesson6/task_6_1.py
+++ lesson6/task_6_1.py
@@ -1,17 +1,25 @@
 #Task 1
 #Make a program that has some sentence (a string) on input and returns a dict containing
 # all unique words as keys and the number of occurrences as values.
-txt_str = input('Pleaase insert a text')
-txt_dict = {}
-txt_str = txt_str.split()
-symbols = [',', '.', '!', '?']
-for item in txt_str:
-    if (item[-1]) in symbols:
-        item = item[:-1]
 
+
+txt_str = input('Pleaase insert a text: ')
+txt_str = txt_str.lower()
+txt_dict = {}
+symbols = ['.',',','!','?',':',';','(',')','{','}','/','@',
+           '#','$','%','^','&','*','-','_','=','+']
+
+for symbol in symbols:
+    if symbol in txt_str:
+        txt_str = txt_str.replace(symbol, ' ')
+        print(symbol)
+txt_str = txt_str.split()
+print(txt_str)
+
+for item in txt_str:
     if item in txt_dict:
         txt_dict[item] = txt_dict[item] + 1
     else:
         txt_dict[item] = 1
 
-print(txt_dict)
+print(f'Unique words and count of them is: {txt_dict}')
--- lesson6/task_6_3.py
+++ lesson6/task_6_3.py
@@ -1,5 +1,6 @@
 #Use a list comprehension to make a list containing tuples (i, j) where `i` goes from 1 to 10 and `j`
 #is corresponding to `i` squared.
 
-new_list = [{i for i in range(1,10 + 1)}, {sum( i**i for i in range(1,10 + 1))} ]
+### Переделал задание
+new_list = [(i, i*i) for i in range(1, 11)]
 print(new_list)
